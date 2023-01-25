5dc6d42fbff89afb739f4a382f01caa7f450f4d3
BganaRoxa <89401543+BganaRoxa@users.noreply.github.com>
2023-01-25 18:05:51 -0400

    correção de bugs nos números

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-const form = document.getElementById('form_numbers')
-const numeroDoisc = document.getElementById('segundoNumero')
+const form = document.getElementById("form_numbers")
 
-let numeroUm = parseFloat(document.getElementById('primeiroNumero').value)
-let numeroDois = parseFloat(document.getElementById('segundoNumero').value)
 
-let formularioValido = false
+
+let numeroUm = parseInt(document.getElementById("primeiroNumero").value);
+let numeroDois = parseInt(document.getElementById("segundoNumero").value);
 
 function numeroValido(numeroUm, numeroDois) {
-    return numeroDois > numeroUm
+    return numeroDois > numeroUm;
 }
 
-form.addEventListener('submit', function(e){
+let formularioValido = false
+form.addEventListener('submit', function(e) {
     e.preventDefault()
 
-    const numeroUmc = document.getElementById('primeiroNumero')
-    const mensagemPositiva = `O preenchimento é válido. O segundo número informado: (${numeroDoisc.value}) é maior que o primeiro número informado: (${numeroUmc.value})`
     
-    formularioValido = numeroValido(numeroUmc.value, numeroDoisc.value)
+    const numeroUmc = parseInt(document.getElementById('primeiroNumero').value)
+    const numeroDoisc = parseInt(document.getElementById('segundoNumero').value)
+    const mensagemPositiva = `O preenchimento é válido. O segundo número informado: (${numeroDoisc}) é maior que o primeiro número informado: (${numeroUmc})`
+    
+    formularioValido = numeroValido(numeroUmc, numeroDoisc)
     if (formularioValido) {
         const containermensagemPositiva = document.querySelector('.mensagem_positiva')
         containermensagemPositiva.innerHTML = mensagemPositiva
@@ -32,9 +34,11 @@ form.addEventListener('submit', function(e){
         containermensagemPositiva.innerHTML = mensagemPositiva
         document.querySelector('.mensagem_negativa').style.display = 'block'
         containermensagemPositiva.style.display = 'none'
+    
         numeroUmc.value = ''
         numeroDoisc.value = ''
-
     }     
     }
 )
+
+
